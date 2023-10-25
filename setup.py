06a408630570fc6a8a05e1a6ceec891d7f78135f
Sinol <sinol.nil@gmail.com>
2023-10-25 12:34:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
-from sys import platform
+from sys import platform,argv
 import os
 
-# This module is to install the config-update command to qutebrwoser
+# This module generate an updateConfig binary file to the specific OS(win/mac).
+# It will auto-delete after executing the commands.
 fileName = "updateConfig"
+whoami = os.environ.get("USER") if os.environ.get("USER")!=None else os.environ.get("USERNAME")
 repo = "https://raw.githubusercontent.com/sinolnil/qutebrowser/main/config.py"
 
+
 def darwinInstall():
+    
     with open (fileName, "w+") as f:
         f.write(f"curl {repo} -o ../config.py")
-    print("darwinInstall download completed")
+    print("Created darwinInstall")
     os.chmod(f"./{fileName}",0o777)
+    print(f"Change {fileName} permission")
     os.system(f"./{fileName}")
+    print(f"Execute {fileName}")
 
+def win32Install():
+    
+    with open (fileName + '.bat', "w+") as f:
+        f.write(f"@echo on\n\
+echo 'Downloading script from github...'\n\
+curl {repo} -o C:\\Users\\{whoami}\\AppData\\Roaming\\qutebrowser\\config\\config.py --ssl-no-revoke")
+    print("Created win32Install")
+
+    
 if platform == "linux" or platform == "linux2":
     pass
 
 elif platform == "darwin": # OS X
+    print("Detected Mac OS")
     darwinInstall()
 
 elif platform == "win32": # Windows
-    pass
+    print("Detected Windows OS")
+    win32Install()
 
+os.remove(argv[0]) #self-delete
