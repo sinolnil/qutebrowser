3eecf26aa0a9bb284159ac1b236a1bde6adb7448
Sinol <sinol.nil@gmail.com>
2023-10-25 16:25:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from sys import platform,argv
 import os
-
 # This module generate an updateConfig binary file to the specific OS(win/mac).
-
-
 REPO = "https://raw.githubusercontent.com/sinolnil/qutebrowser/main/config.py"
 WHOAMI = os.environ.get("USER") if os.environ.get("USER")!=None else os.environ.get("USERNAME")
 fileName = "updateConfig"
 win32Path = os.path.join('C:',os.sep,'Users',WHOAMI,'AppData','Roaming','qutebrowser','config')
 macPath = ""
 
+
+def writeFile(path,fileName,script,mode="w+"):
+    print(f"Paht:{path} FileName:{fileName}")
+    print("Writing file...")
+    try:
+        with open(path + '/' + fileName,mode) as f:
+            f.write(script)
+    except Exception as e:
+        print(e)
 def darwinInstall():
     try:
         with open (fileName, "w+") as f:
@@ -20,34 +26,31 @@ def darwinInstall():
         print(e)
 
 def win32Install():
-    file = os.path.join(win32Path,'userscripts')
-    try:
-        if not os.path.exists(file):
-            print(f'Create {file}')
-            os.makedirs(file)
-    except Exception as e:
-        print(e)
-        
-    file = os.path.join(file,fileName)
-    import subprocess # for execute bat command
-    try:
-        with open ( file, "w+") as f:
-            f.write(f"@echo on\n"
-                    "echo 'Downloading script from github...'\n"
-                    f"curl {REPO} -o {win32Path}/config.py --ssl-no-revoke")
-    except Exception as e:
-        print(e)
-        
-    print(f"PASS..Created win32Install at {file}")
-    
-    try:
-        subprocess.run(file)
-        print(f"PASS..Execute {fileName}")
-    except Exception as e:
-        print(f"FAIL..Something went wrong. [{e}]")
+	path = os.path.join(win32Path,'userscripts')
+	try:
+		if not os.path.exists(path):
+			print(f'Create {path}')
+			os.makedirs(path)
+	except Exception as e:
+		print(e)
+	updateConfig = (f"@echo on\n"
+				"echo 'Downloading script from github...'\n"
+				f"curl {REPO} -o {win32Path}/config.py --ssl-no-revoke")
 
+	openConfig = (f"@echo on\n"
+               "start C:/Users/%USERNAME%/AppData/Roaming/qutebrowser/config")
 
-    
+	writeFile(path,fileName,updateConfig)
+	writeFile(path,'openConfig.bat',openConfig)
+
+	try:
+		import subprocess # for execute bat command
+		subprocess.run(os.path.join(path,fileName))
+		print(f"PASS..Execute {fileName}")
+        
+	except Exception as e:
+		print(f"FAIL..Something went wrong. [{e}]")
+
 if platform == "linux" or platform == "linux2":
     print("[Detected Linux]")
     print("This should not be happen")
@@ -61,4 +64,3 @@ elif platform == "win32": # Windows
     print("[Detected Windows OS]")
     fileName = fileName + '.bat'
     win32Install()
-
