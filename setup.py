df4f1dbe69ca9695e2296c7e266b77207cf94534
Sinol <sinol.nil@gmail.com>
2023-10-26 21:59:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,48 +3,56 @@ import os
 # This module generate an updateConfig binary file to the specific OS(win/mac).
 REPO = "https://raw.githubusercontent.com/sinolnil/qutebrowser/main/config.py"
 WHOAMI = os.environ.get("USER") if os.environ.get("USER")!=None else os.environ.get("USERNAME")
-fileName = "updateConfig"
 win32Path = os.path.join('C:',os.sep,'Users',WHOAMI,'AppData','Roaming','qutebrowser','config')
-macPath = ""
+macPath = f"/Users/{WHOAMI}/.qutebrowser"
 
 
 def writeFile(path,fileName,script,mode="w+"):
-    print(f"Paht:{path} FileName:{fileName}")
-    print("Writing file...")
     try:
-        with open(path + '/' + fileName,mode) as f:
+        if not os.path.exists(path):
+            print(f"Create {path}")
+            os.makedirs(path)
+        else:
+            print("File found")
+    except Exception as e:
+        print(f"Exception [writeFile] {e}")
+
+
+    try:
+        with open(os.path.join(path,fileName),mode) as f:
+            print(f"Writing file...@ {path} file:{fileName}")
             f.write(script)
     except Exception as e:
         print(e)
+
 def darwinInstall():
+    path = os.path.join(macPath,'userscripts')
+    updateConfig = (f"curl {REPO} -o ../config.py")
+    writeFile(path,'updateConfig',updateConfig)
+
+    #execute updateConfig
     try:
-        with open (fileName, "w+") as f:
-            f.write(f"curl {REPO} -o ../config.py")
-        os.chmod(f"./{fileName}",0o777)
-        os.system(f"./{fileName}")
+        #change the permission
+        f = "updateConfig"
+        os.chmod(f"./{f}",0o777)
+        os.system(f"./{f}") 
     except Exception as e:
         print(e)
 
 def win32Install():
 	path = os.path.join(win32Path,'userscripts')
-	try:
-		if not os.path.exists(path):
-			print(f'Create {path}')
-			os.makedirs(path)
-	except Exception as e:
-		print(e)
 	updateConfig = (f"@echo on\n"
 				f"curl {REPO} -o {win32Path}/config.py --ssl-no-revoke")
 
 	openConfig = (f"@echo on\n"
                "start C:/Users/%USERNAME%/AppData/Roaming/qutebrowser/config")
 
-	writeFile(path,fileName,updateConfig)
-	writeFile(path,'openConfig.bat',openConfig)
+	writeFile(path,"updateConfig.bat",updateConfig)
+	writeFile(path,"openConfig.bat",openConfig)
 
 	try:
 		import subprocess # for execute bat command
-		subprocess.run(os.path.join(path,fileName))
+		subprocess.run(os.path.join(path,'updateConfig.bat'))
 		print(f"PASS..Execute {fileName}")
         
 	except Exception as e:
@@ -61,5 +69,5 @@ elif platform == "darwin": # OS X
 
 elif platform == "win32": # Windows
     print("[Detected Windows OS]")
-    fileName = fileName + '.bat'
     win32Install()
+
